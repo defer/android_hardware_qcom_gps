74c338834ce61ef8daade208ee36a35186a7f2b9
Stephen Li <stephenl@codeaurora.org>
2011-03-18 17:30:14 -0700

    gps: daemon: add FEATURE_GNSS_BIT_API for SVLTE
    Change-Id: I0bff9dc70cb553046725316d8e5e1c9a12d5bc21

--- loc_api/libloc_api_50001/Android.mk
+++ loc_api/libloc_api_50001/Android.mk
@@ -3,6 +3,12 @@ ifneq ($(BUILD_TINY_ANDROID),true)
 ifeq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION),50001)
 AMSS_VERSION:=6356
 
+BIT_ENABLED_PRODUCT_LIST := msm7630_fusion
+BIT_ENABLED_PRODUCT_LIST += msm8660_surf
+ifneq (, $(filter $(BIT_ENABLED_PRODUCT_LIST), $(QCOM_TARGET_PRODUCT)))
+FEATURE_GNSS_BIT_API := true
+endif # QCOM_TARGET_PRODUCT
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -26,6 +32,7 @@ LOCAL_SRC_FILES += \
     gps.c
 
 ifeq ($(FEATURE_GNSS_BIT_API), true)
+LOCAL_CFLAGS += -DFEATURE_GNSS_BIT_API
 LOCAL_SRC_FILES += \
     loc_eng_dmn_conn.cpp \
     loc_eng_dmn_conn_handler.cpp \
@@ -54,23 +61,16 @@ LOCAL_C_INCLUDES:= \
 
 ifeq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION),50001)
 LOCAL_C_INCLUDES += \
-	$(TARGET_OUT_HEADERS)/libloc_api-rpc-qc \
-	$(TARGET_OUT_HEADERS)/libloc_api-rpc-qc/rpc_inc \
+        $(TARGET_OUT_HEADERS)/libloc_api-rpc-qc \
+        $(TARGET_OUT_HEADERS)/libloc_api-rpc-qc/rpc_inc \
         $(TARGET_OUT_HEADERS)/loc_api/rpcgen/inc \
         $(TARGET_OUT_HEADERS)/libcommondefs/rpcgen/inc \
         hardware/msm7k/librpc
 else
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/libloc_api-rpc \
-	$(TARGET_OUT_HEADERS)/libloc_api-rpc/inc
+        $(TARGET_OUT_HEADERS)/libloc_api-rpc/inc
 endif
-
-ifeq ($(FEATURE_GNSS_BIT_API), true)
-LOCAL_C_INCLUDES += \
-        $(LOCAL_PATH)/../../../common/inc \
-        $(LOCAL_PATH)/../../../oncrpc/inc \
-        $(LOCAL_PATH)/../../../diag/include
-endif # FEATURE_GNSS_BIT_API
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
